d6bbbd2be9df8e96350a64e4759dde7ac0e6a800
Caleb <133560146+CalebStark@users.noreply.github.com>
2025-04-13 02:12:31 -0500

    Delete MoffatBayMarina directory

--- MoffatBayMarina/src/main/webapp/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();  // Start the session to clear data
-session_unset();  // Unset all session variables
-session_destroy();  // Destroy the session completely
-header("Location: login.php");  // Redirect to the login page
-exit();
-?>
